5b66f31915e0b9b6ea168d882338ed246ef0c42c
Olasupo "Paddy" Odebiyi <olasupo.dev@gmail.com>
2022-03-22 17:19:20 +0000

    Let's add in navigation

--- src/screens/landing/Welcome.Screen.tsx
+++ src/screens/landing/Welcome.Screen.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
+import {Button} from 'react-native';
 
 import {BaseFont} from '../../components/Font/BaseFont';
 import {Page} from '../../components/Page/Page';
 
-export const WelcomeScreen = () => {
+interface Props {
+  something?: string;
+}
+
+export const WelcomeScreen: React.FC<Props> = () => {
   return (
     <Page onPress={() => console.log('Tappable Screen!')}>
       <BaseFont variant="title">Welcome Screen</BaseFont>
-      <BaseFont variant="small-caps">This is a subheading</BaseFont>
+      <BaseFont variant="small-caps">This is a good subheading</BaseFont>
       <BaseFont variant="paragraph">This is a paragraph</BaseFont>
+      <Button title="what a button" />
     </Page>
   );
 };
